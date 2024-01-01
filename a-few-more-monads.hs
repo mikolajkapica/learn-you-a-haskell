cd86ccea2442c5b3a305c24d6395a8e33f6a719d
Mikołaj Kapica <mikolajkapica@mail.com>
2024-01-01 19:51:08 +0100

    finished all

--- a-few-more-monads.hs
+++ a-few-more-monads.hs
@@ -3,7 +3,9 @@
 import Control.Monad.Writer
 import Data.Bifunctor (second)
 import GHC.Iface.Ext.Debug (Diff)
+import Control.Monad.State
 import System.Clock (getTime, Clock(Monotonic))
+import Data.List
 
 countdown :: Int -> Writer [String] Int
 countdown n = do
@@ -43,17 +45,6 @@ finalCountDown x = do
     finalCountDown (x-1)
     tell [show x]
 
-timeit f x = do
-    start <- getTime Monotonic
-    result <- f x
-    end <- getTime Monotonic
-    return (result, end - start)
-
-main = do
-    putStrLn "finalCountDown 5000 List"
-    print (snd $ timeit runWriter (finalCountDown 5000))
-    return ()
-
 newtype DiffList a = DiffList { getDiffList :: [a] -> [a] }
 
 toDiffList :: [a] -> DiffList a
@@ -69,3 +60,41 @@ instance Monoid (DiffList a) where
     mempty = DiffList (\xs -> [] ++ xs)
 
 (|>) x f = f x
+
+
+type Stack = [Int]
+
+pop :: State Stack Int
+pop = state $ \(x:xs) -> (x,xs)
+
+push :: Int -> State Stack ()
+push a = state $ \xs -> ((), a:xs)
+
+stackManip :: State Stack Int  
+stackManip = do  
+    push 3  
+    pop  
+    pop  
+
+
+
+
+-- RPN calculator
+
+solveRPN st = foldM foldingFunction [] (words st)
+
+foldingFunction :: [Float] -> String -> Maybe [Float]
+foldingFunction (x:y:ys) "*" = return $ (x * y):ys
+foldingFunction (x:y:ys) "+" = return $ (x + y):ys
+foldingFunction (x:y:ys) "-" = return $ (y - x):ys
+foldingFunction (x:y:ys) "/" = return $ (y / x):ys
+foldingFunction (x:y:ys) "^" = return $ (y ** x):ys
+foldingFunction (x:xs) "ln"  = return $ log x:xs
+foldingFunction xs "sum"     = return $ [sum xs]
+foldingFunction xs numberString = liftM (:xs) (readMaybe numberString) 
+
+readMaybe :: (Read a) => String -> Maybe a
+readMaybe st = case reads st of [(x,"")] -> Just x
+                                _ -> Nothing
+
+
